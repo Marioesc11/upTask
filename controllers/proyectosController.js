9d854a72928a0240c5a06a75db0d9c9989372567
Marioesc11 <mario.escudero@tsoftlatam.com>
2022-03-08 19:49:33 -0300

    "actualizacion"

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -31,7 +31,7 @@ exports.nuevoProyecto = async (req,res) => {
     let errores = [];
 
     if(!nombre) {
-        errores.push({'texto' : 'agrega textos pa'})
+        errores.push({'texto' : 'agrega textosss ahre xd'})
     }
 
     if(errores.length > 0){
